dfda99e1fa1b50dbd0dccde718826f202e2dea61
John-Masi <johnrmasi@gmail.com>
2026-02-14 21:59:24 -0600

    CMake Build edit

---
+++ build/CMakeFiles/file_extension_reading.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/johm01/file_extension_reading/file_extension_reading"
+  "/home/johm01/file_extension_reading/file_extension_reading.pdb"
+  "CMakeFiles/file_extension_reading.dir/src/file.cpp.o"
+  "CMakeFiles/file_extension_reading.dir/src/file.cpp.o.d"
+  "CMakeFiles/file_extension_reading.dir/src/main.cpp.o"
+  "CMakeFiles/file_extension_reading.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_extension_reading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
